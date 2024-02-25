bb6e95a1c9e5d94e7c22ee64c6d3a45435c111db
Huneshwar97 <huneshwaryadav123@gmail.com>
2024-02-25 21:53:40 +0530

    added skeleton for auth service

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const {PORT} = require('./config/serverConfig');
+
+const app = express();
+
+const prepareAndStartServer = () => {
+
+    app.listen(PORT,() => {
+        console.log(`Server Started at ${PORT}`);
+    })
+}
+
+prepareAndStartServer();
